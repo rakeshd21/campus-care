df4e35a7cbe19490077b9c9062acea6c4a87481d
Rakesh Dandoti <rakeshsdandoti4620@gmail.com>
2026-03-20 11:19:31 +0530

    Update Firebase configuration with real credentials
    Updated Firebase configuration with actual API key and messaging sender ID.

--- firebase.js
+++ firebase.js
@@ -6,11 +6,11 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "put api id",
+  apiKey: "AIzaSyATDI89sphFyKNY7l7vG3VR8UO7e2nOfTQ",
   authDomain: "campuscare-28f5d.firebaseapp.com",
   projectId: "campuscare-28f5d",
   storageBucket: "campuscare-28f5d.firebasestorage.app",
-  messagingSenderId: "sender id",
+  messagingSenderId: "1012511944489",
   appId: "1234",
   measurementId: "G-8EK3C4STF9"
 };
